25afbdfb42cfa6f5d909d88e1f44c5153179e1da
Mehul <mehulprajapati2802@gmail.com>
2025-09-30 16:52:24 +0530

    add console logs

--- app.js
+++ app.js
@@ -33,5 +33,17 @@ function add(numbers) {
     return sum;
 }
 
+console.log(add(""));
+console.log(add("1"));
+console.log(add("1,2"));
+console.log(add("1\n2,3"));
+console.log(add("//;\n1;2"));
+
+try {
+  add("1,-2");
+}
+catch (e) {
+  console.error(e.message);
+}
 
 module.exports = add;
