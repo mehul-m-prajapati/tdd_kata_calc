9bbae0384c2755fc8db88e6d81726abbec7e59fd
Mehul <mehulprajapati2802@gmail.com>
2025-09-30 15:36:18 +0530

    add 2nd test case

--- app.test.js
+++ app.test.js
@@ -3,3 +3,7 @@ const add = require('./app');
 test('returns 0 for empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('returns number when a single number is passed', () => {
+    expect(add("5")).toBe(5);
+})
