b531ab7e180c5cc409eb092f2f13e17966db75f1
Mehul <mehulprajapati2802@gmail.com>
2025-09-30 15:44:11 +0530

    add test case of multiple comma separated numbers

--- app.test.js
+++ app.test.js
@@ -11,3 +11,7 @@ test('returns number when a single number is passed', () => {
 test('returns sum of two comma separated numbers', () => {
   expect(add("1, 2")).toBe(3);
 });
+
+test('returns sum of multiple comma separated numbers', () => {
+  expect(add("1, 4, 6, 9")).toBe(20);
+});
