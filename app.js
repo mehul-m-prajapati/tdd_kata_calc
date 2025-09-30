827e58888f335dc3bfa4528089f995f65e297c59
Mehul <mehulprajapati2802@gmail.com>
2025-09-30 15:42:08 +0530

    sum up values to pass 3rd test case

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ function add(numbers) {
     if (numbers === "")
         return 0;
 
-    return parseInt(numbers);
+    const arr_num = numbers.split(',');
+    let sum = 0;
+
+    for (let i = 0; i < arr_num.length; i++) {
+        sum += parseInt(arr_num[i]);
+    }
+
+    return sum;
 }
 
 
