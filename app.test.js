a61bfbe42bcbec983d53b06f83311a3eeea6500b
Mehul <mehulprajapati2802@gmail.com>
2025-09-30 15:49:21 +0530

    add test case of custom delimiter

--- app.test.js
+++ app.test.js
@@ -18,4 +18,8 @@ test('returns sum of multiple comma separated numbers', () => {
 
 test('support newline as delimiter', () => {
     expect(add("1\n2,3")).toBe(6);
-})
+});
+
+test('supports custom delimiters', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
