0565c6c5e9d749fe6c48b00d299fe9b31a5c15ac
Mehul <mehulprajapati2802@gmail.com>
2025-09-30 16:35:10 +0530

    add code to support custom delimiters

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ function add(numbers) {
     if (numbers === "")
         return 0;
 
-    const normalized = numbers.replace(/\n/g, ',');
-    const arr_num = normalized.split(',');
+    let delimiters = [",", "\n"];
+    let num_str = numbers;
+
+    // e.g. "//;\n1;2;3"
+    if (numbers.startsWith("//")) {
+        const p = numbers.split("\n");
+        delimiters = [p[0].substring(2)];
+        num_str = p[1];
+    }
+
+    for (const delim of delimiters) {
+        num_str = num_str.split(delim).join(',');
+    }
+
+    const arr_num = num_str.split(',');
     let sum = 0;
 
     for (let i = 0; i < arr_num.length; i++) {
-        sum += parseInt(arr_num[i]);
+        sum += parseInt(arr_num[i].trim());
     }
 
     return sum;
--- app.test.js
+++ app.test.js
@@ -20,6 +20,6 @@ test('support newline as delimiter', () => {
     expect(add("1\n2,3")).toBe(6);
 });
 
-test('supports custom delimiters', () => {
+test('support custom delimiters', () => {
   expect(add("//;\n1;2")).toBe(3);
 });
